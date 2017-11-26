03a20b871797120a2665ae1a2342741deead6802
Eirin <eiringonzales@gmail.com>
2017-11-26 15:05:36 +0800

    Removed something

--- app/routes/index.js
+++ app/routes/index.js
@@ -207,10 +207,6 @@ module.exports = function(app, passport) {
 
   });
 
-  app.route('/').get(function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
-
   app.use('/api/v1', router);
 
 };
